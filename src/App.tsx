60203c90537a246efc7483d160b1d057c18691cd
Ly <lyttt.dev@gmail.com>
2022-05-19 23:40:34 +0700

    refactor: pass data as object to article card component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Footer, Header, ArticleCard } from './components';
-import IndexPage from './pages';
-import ArticleDetailPage from './pages/ArticleDetailPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Footer, Header, ArticleCard } from "./components";
+import IndexPage from "./pages";
+import ArticleDetailPage from "./pages/ArticleDetailPage";
 
-import { library } from '@fortawesome/fontawesome-svg-core'
+import { library } from "@fortawesome/fontawesome-svg-core";
 // import { fab } from '@fortawesome/free-brands-svg-icons'
-import { faEye } from '@fortawesome/free-solid-svg-icons'
-import { faSquareCheck} from '@fortawesome/free-regular-svg-icons'
-library.add(faEye, faSquareCheck)
+import { faEye } from "@fortawesome/free-solid-svg-icons";
+import { faSquareCheck } from "@fortawesome/free-regular-svg-icons";
+library.add(faEye, faSquareCheck);
 
 const App = () => {
+
+  let article_card_mock_data = {
+    title:
+      "Weekly News Quiz for Students: Roe v. Wade, Drought, Kentucky Derby",
+    date: new Date(),
+    numberOfView: 100,
+    numberOfQuestion: 9,
+    // change url -> not save to local
+    thumbnailUrl: "https://static01.nyt.com/images/2022/04/25/climate/00cli-turfban1-LNNQ/merlin_204818043_fa28d456-8e9c-40d5-a4ad-2634453cdbb9-mediumThreeByTwo252.jpg?quality=100&auto=webp",
+  };
+
   return (
     <div className="App">
-      <ArticleCard title='Weekly News Quiz for Students: Roe v. Wade, Drought, Kentucky Derby' date={new Date()} numberOfView={100} numberOfQuestion={9} thumbnailUrl="/images/thumb.png"></ArticleCard>
+      <ArticleCard data={article_card_mock_data}></ArticleCard>
     </div>
   );
   // return (
@@ -27,6 +38,6 @@ const App = () => {
   //     </Router>
   //   </>
   // )
-}
+};
 
 export default App;
--- src/components/ArticleCard/index.tsx
+++ src/components/ArticleCard/index.tsx
@@ -3,40 +3,38 @@ import styles from "./ArticleCard.module.scss";
 // get our fontawesome imports
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import { faCoffee, faEye, faSquareCheck } from '@fortawesome/free-solid-svg-icons'
+import { FaBeer } from 'react-icons/fa';
 
 interface ArticleCardProps {
-  title: string;
-  date: Date;
-  numberOfView: number;
-  numberOfQuestion: number;
-  thumbnailUrl: string;
+  data: {
+    title: string;
+    date: Date;
+    numberOfView: number;
+    numberOfQuestion: number;
+    thumbnailUrl: string;
+  }
+
 }
 
-const ArticleCard: React.FC<ArticleCardProps> = ({
-  title,
-  date,
-  numberOfView,
-  numberOfQuestion,
-  thumbnailUrl,
-}) => {
+const ArticleCard: React.FC<ArticleCardProps> = ({data}) => {
   return (
     <div className={styles["article-card"]}>
-      <h3 className={styles["article-title"]}>{title}</h3>
+      <h3 className={styles["article-title"]}>{data.title}</h3>
       {/* Article views */}
       <div className={styles["article-views"]}>
         <FontAwesomeIcon icon="eye" className={styles["article-views-icon"]}/>
-        <div className={styles["article-views-content"]}> {numberOfView} views</div>
+        <div className={styles["article-views-content"]}> {data.numberOfView} views</div>
       </div>
 
       {/* Article number of questions */}
       <div className={styles["article-questions"]}>
         <FontAwesomeIcon icon={['far', 'square-check']} className={styles["article-questions-icon"]}/>
-        <div className={styles["article-questions-content"]}> {numberOfQuestion} questions</div>
+        <div className={styles["article-questions-content"]}> {data.numberOfQuestion} questions</div>
       </div>
 
 
-      <img className={styles["thumbnail-image"]} src={thumbnailUrl} alt="" />
-      <div className={styles["article-date"]}>{date.toDateString()}</div>
+      <img className={styles["thumbnail-image"]} src={data.thumbnailUrl} alt="" />
+      <div className={styles["article-date"]}>{data.date.toDateString()}</div>
     </div>
   );
 };

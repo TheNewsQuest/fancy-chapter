021ef1dff516a20c353bea03003c93837276837c
Ly <lyttt.dev@gmail.com>
2022-05-21 22:33:23 +0700

    refactor: create seperate page for testing purpose

--- src/App.tsx
+++ src/App.tsx
@@ -9,29 +9,24 @@ const App = () => {
     title:
       "Weekly News Quiz for Students: Roe v. Wade, Drought, Kentucky Derby",
     date: new Date(),
-    numberOfView: 100,
-    numberOfQuestion: 9,
-    // change url -> not save to local
+    viewCount: 100,
+    questionCount: 9,
     thumbnailUrl: "https://static01.nyt.com/images/2022/04/25/climate/00cli-turfban1-LNNQ/merlin_204818043_fa28d456-8e9c-40d5-a4ad-2634453cdbb9-mediumThreeByTwo252.jpg?quality=100&auto=webp",
   };
 
   return (
-    <div className="App">
-      <ArticleCard data={article_card_mock_data}></ArticleCard>
-    </div>
-  );
-  // return (
-  //   <>
-  //     <Router>
-  //       <Header />
-  //       <Routes>
-  //         <Route path="/" element={<IndexPage />} />
-  //         <Route path="/articles/:id" element={<ArticleDetailPage />} />
-  //       </Routes>
-  //       <Footer />
-  //     </Router>
-  //   </>
-  // )
+    <>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/article-card-sample" element={<ArticleCard data={article_card_mock_data}></ArticleCard>} />
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/articles/:id" element={<ArticleDetailPage />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </>
+  )
 };
 
 export default App;

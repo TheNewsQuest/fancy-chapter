b8689f157e1a8c5a72640d0fd8130a2c0065b3f6
Ly <lyttt.dev@gmail.com>
2022-05-21 22:33:43 +0700

    refactor: make ArticleCard responsive

--- src/components/ArticleCard/index.tsx
+++ src/components/ArticleCard/index.tsx
@@ -1,38 +1,56 @@
 import React from "react";
 import styles from "./ArticleCard.module.scss";
-import { FaEye, FaRegCheckSquare } from 'react-icons/fa';
-
+import { FaEye, FaRegCheckSquare } from "react-icons/fa";
+import { format } from 'date-fns';
 
 interface ArticleCardProps {
   data: {
     title: string;
     date: Date;
-    numberOfView: number;
-    numberOfQuestion: number;
+    viewCount: number;
+    questionCount: number;
     thumbnailUrl: string;
-  }
-
+  };
 }
 
-const ArticleCard: React.FC<ArticleCardProps> = ({data}) => {
+const ArticleCard: React.FC<ArticleCardProps> = ({ data }) => {
   return (
     <div className={styles["article-card"]}>
-      <h3 className={styles["article-title"]}>{data.title}</h3>
-      {/* Article views */}
-      <div className={styles["article-views"]}>
-        <FaEye className={styles["article-views-icon"]} />
-        <div className={styles["article-views-content"]}> {data.numberOfView} views</div>
+      <div className={styles["left"]}>
+        <div className={styles["article-date"]}>{format(data.date, 'dd MMM yyyy')}</div>
+      </div>
+      <div className={styles["middle"]}>
+        <a href="/" className={styles["article-title"]}>
+          {data.title}
+        </a>
+        <div className={styles["article-statistics"]}>
+          {/* Article views */}
+          <div className={styles["article-views"]}>
+            <FaEye className={styles["article-views-icon"]} />
+            <div className={styles["article-views-content"]}>
+              {" "}
+              {data.viewCount} views
+            </div>
+          </div>
+
+          {/* Article number of questions */}
+          <div className={styles["article-questions"]}>
+            <FaRegCheckSquare className={styles["article-questions-icon"]} />
+            <div className={styles["article-questions-content"]}>
+              {" "}
+              {data.questionCount} questions
+            </div>
+          </div>
+        </div>
       </div>
 
-      {/* Article number of questions */}
-      <div className={styles["article-questions"]}>
-        <FaRegCheckSquare className={styles["article-questions-icon"]}/>
-        <div className={styles["article-questions-content"]}> {data.numberOfQuestion} questions</div>
+      <div className={styles["right"]}>
+        <img
+          className={styles["thumbnail-image"]}
+          src={data.thumbnailUrl}
+          alt=""
+        />
       </div>
-
-
-      <img className={styles["thumbnail-image"]} src={data.thumbnailUrl} alt="" />
-      <div className={styles["article-date"]}>{data.date.toDateString()}</div>
     </div>
   );
 };

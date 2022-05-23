0a0c1864e70a46ac35802015236984d92f5c74e2
Ly <lyttt.dev@gmail.com>
2022-05-23 14:35:07 +0700

    feat: init multiple choice component

---
+++ src/components/MultipleChoice/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./MultipleChoice.module.scss";
+
+const MultipleChoice: React.FC = () => {
+  return (
+    <>
+      <div>Multiple choice questions</div>
+    </>
+  );
+};
+
+export default MultipleChoice;

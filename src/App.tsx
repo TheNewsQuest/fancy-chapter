e3aaca3323ebace2f75a613a0d65e4d7f5efa3b8
Ly <lyttt.dev@gmail.com>
2022-05-24 00:58:06 +0700

    feat: done layout for multiple choice

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,55 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Footer, Header } from './components';
-import IndexPage from './pages';
-import ArticleDetailPage from './pages/ArticleDetailPage';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Footer, Header } from "./components";
+import MultipleChoiceQuestion from "./components/MultipleChoice";
+import IndexPage from "./pages";
+import ArticleDetailPage from "./pages/ArticleDetailPage";
 
 const App = () => {
   let article_card_mock_data = {
     title:
-      'Weekly News Quiz for Students: Roe v. Wade, Drought, Kentucky Derby',
+      "Weekly News Quiz for Students: Roe v. Wade, Drought, Kentucky Derby",
     date: new Date(),
     viewCount: 100,
     questionCount: 9,
     thumbnailUrl:
-      'https://static01.nyt.com/images/2022/04/25/climate/00cli-turfban1-LNNQ/merlin_204818043_fa28d456-8e9c-40d5-a4ad-2634453cdbb9-mediumThreeByTwo252.jpg?quality=100&auto=webp',
+      "https://static01.nyt.com/images/2022/04/25/climate/00cli-turfban1-LNNQ/merlin_204818043_fa28d456-8e9c-40d5-a4ad-2634453cdbb9-mediumThreeByTwo252.jpg?quality=100&auto=webp",
   };
+
+  let multiple_choice_mock_data = [
+    {
+      question: "Who created Java?",
+      answers: [
+        "Guido van Rossum",
+        "David Beckham",
+        "David Copperfield",
+        "Robin Van Persie",
+      ],
+      correctAnswerIndex: 2,
+    },
+    {
+      question: "Who created Java?",
+      answers: [
+        "Guido van Rossum",
+        "David Beckham",
+        "David Copperfield",
+        "Robin Van Persie",
+      ],
+      correctAnswerIndex: 2,
+    },
+  ];
 
   return (
     <>
       <Router>
         <Header />
         <Routes>
+          <Route
+            path="/multiple-choice"
+            element={
+              <MultipleChoiceQuestion list={multiple_choice_mock_data} />
+            }
+          />
+          {/* <Route path="/multiple-choice" element={<MultipleChoice data={multiple_choice_mock_data} />} /> */}
           <Route path="/" element={<IndexPage />} />
           <Route path="/articles/:id" element={<ArticleDetailPage />} />
         </Routes>
--- src/components/MultipleChoice/index.tsx
+++ src/components/MultipleChoice/index.tsx
@@ -1,12 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./MultipleChoice.module.scss";
 
-const MultipleChoice: React.FC = () => {
-  return (
-    <>
-      <div>Multiple choice questions</div>
-    </>
-  );
+interface MultipleChoiceObject {
+  question: string;
+  answers: string[];
+  correctAnswerIndex: number;
+}
+
+interface MultipleChoiceProps {
+  list: MultipleChoiceObject[];
+}
+
+const MultipleChoice: React.FC<MultipleChoiceProps> = ({ list }) => {
+  // const [value, setValue] = useState(1);
+
+  // const onChange = (e: RadioChangeEvent) => {
+  //   console.log("radio checked", e.target.value);
+  //   setValue(e.target.value);
+  // };
+
+  const question = list.map((item, index) => {
+    return (
+      <div className={styles["multiple-choice-container"]}>
+        <div className={styles["multiple-choice-question"]}>
+          {index + 1}. {item.question}
+        </div>
+        <div>
+          <ul>
+            {item.answers.map((ans, index) => {
+              return (
+                <li>
+                  <label
+                    htmlFor={"option-" + index}
+                    className={styles["container"]}
+                  >
+                    <input type="radio" id={"option-" + index} />
+                    <span className={styles["checkmark"]}></span>
+                    {ans}
+                  </label>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    );
+  });
+
+  return <>{question}</>;
 };
 
 export default MultipleChoice;

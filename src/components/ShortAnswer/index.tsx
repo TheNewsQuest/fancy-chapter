2c05140d505f918e4252aca3dedbc38fcc59d1a1
Ly <lyttt.dev@gmail.com>
2022-05-25 02:21:35 +0700

    feat: init short answer component

---
+++ src/components/ShortAnswer/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./ShortAnswer.module.scss";
+
+const ShortAnswer: React.FC = () => {
+    return (
+        <div></div>
+    );
+}
+
+export default ShortAnswer

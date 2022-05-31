1f8e8c47dbcefddaf58e756143ccb83c3938b650
Ly <lyttt.dev@gmail.com>
2022-05-31 13:47:35 +0700

    feat: add css for tab

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Footer, Header } from './components';
+import SwitchTab from './components/SwitchTab';
 import IndexPage from './pages';
 import ArticleDetailPage from './pages/ArticleDetailPage';
 
@@ -49,6 +50,7 @@ const App = () => {
       <Router>
         <Header />
         <Routes>
+          <Route path="/tab" element={<SwitchTab />} />
           <Route path="/" element={<IndexPage />} />
           <Route path="/articles/:id" element={<ArticleDetailPage />} />
         </Routes>
--- src/components/SwitchTab/index.tsx
+++ src/components/SwitchTab/index.tsx
@@ -1,10 +1,57 @@
 import React from "react";
 import styles from "./SwitchTab.module.scss";
+import { Tabs } from "antd";
+import MultipleChoice from "../MultipleChoice";
+import ShortAnswer from "../ShortAnswer";
+
+const { TabPane } = Tabs;
+let short_answer_mock_data = [
+  {
+    question: "What is the famous genre of music in 2022?",
+    answer: "Pop",
+  },
+  {
+    question: "How old is the Earth?",
+    answer: "4.543 billion years",
+  },
+];
+let multiple_choice_mock_data = [
+  {
+    question: "Who created Python?",
+    answers: [
+      "Guido van Rossum",
+      "David Beckham",
+      "David Copperfield",
+      "Robin Van Persie",
+    ],
+    correctAnswerIndex: 2,
+    // userChoice: -1
+  },
+  {
+    question: "Is Python a programming language or an animal?",
+    answers: ["A programming language", "An animal"],
+    correctAnswerIndex: 1,
+    // userChoice: -1
+  },
+];
+
+const onChange = (key: string) => {
+  console.log(key);
+};
 
 const SwitchTab = () => {
-    return (
-        <div></div>
-    );
-}
+  return (
+      <div className="card-container">
+        <Tabs type="card" centered={true} defaultActiveKey="1" onChange={onChange}>
+          <TabPane tab="Multiple choice" key="1">
+            <MultipleChoice list={multiple_choice_mock_data} />
+          </TabPane>
+          <TabPane tab="Short Answer" key="2">
+            <ShortAnswer shortAnswerList={short_answer_mock_data} />
+          </TabPane>
+        </Tabs>
+      </div>
+  );
+};
 
 export default SwitchTab;

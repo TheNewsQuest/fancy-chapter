bcb15da8920dcccfa7a2b491bb1861e0db2be2e4
bietdoikiem <minhlucky2408@gmail.com>
2022-05-31 12:27:30 +0700

    feat: add backtop butotn

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { BackTop } from 'antd';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { Footer, Header } from './components';
 import IndexPage from './pages';
@@ -52,6 +53,7 @@ const App = () => {
           <Route path="/" element={<IndexPage />} />
           <Route path="/articles/:id" element={<ArticleDetailPage />} />
         </Routes>
+        <BackTop />
         <Footer />
       </Router>
     </>

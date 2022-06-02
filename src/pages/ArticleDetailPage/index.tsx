a24f8918a21c88da63854fc3db0ab66ee0f1852c
Ly <lyttt.dev@gmail.com>
2022-06-02 15:52:04 +0700

    feat: qr code layout

--- src/pages/ArticleDetailPage/index.tsx
+++ src/pages/ArticleDetailPage/index.tsx
@@ -1,16 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { notification, Skeleton } from "antd";
 import { NotificationPlacement } from "antd/lib/notification";
 import { useParams } from "react-router-dom";
 import { Container } from "../../components";
 import styles from "./ArticleDetailPage.module.scss";
 import useStore from "../../store/root";
+import { Button, Radio } from "antd";
+import { QrcodeOutlined } from "@ant-design/icons";
+import type { SizeType } from "antd/es/config-provider/SizeContext";
 
 type ArticleDetailParams = {
   id: string;
 };
 
 const ArticleDetailPage: React.FC = () => {
+
   const {
     article,
     initLoading,
@@ -42,14 +46,20 @@ const ArticleDetailPage: React.FC = () => {
 
   const getParagraph = (content: string) => {
     if (content && content.length > 0) {
-      const res = content.split(/\r?\n/)
+      const res = content.split(/\r?\n/);
       return res.map((paragraph) => {
         console.log(paragraph);
-        
-        return <p className={styles["paragraph"]}>{paragraph}</p>
-      })
+
+        return <p className={styles["paragraph"]}>{paragraph}</p>;
+      });
     }
-    return ""
+    return "";
+  };
+
+  const displayQRCodeModal = () => {
+    return (
+      <></>
+    )
   }
 
   return (
@@ -70,21 +80,24 @@ const ArticleDetailPage: React.FC = () => {
               alt="Alt text"
             />
             <div className={styles["content"]}>
-            { 
-
-              getParagraph(article.content)
-              // article.content.split(/\r?\n/).map((paragraph) => {
-              //   return <p className={styles["paragraph"]}>{paragraph}</p>
-              // })
-            }
+              {
+                getParagraph(article.content)
+                // article.content.split(/\r?\n/).map((paragraph) => {
+                //   return <p className={styles["paragraph"]}>{paragraph}</p>
+                // })
+              }
             </div>
+
             {/* TODO: Question Tab */}
+            {/* QR Code*/}
+            <Button type="primary" icon={<QrcodeOutlined />} size={"large"} onClick={() => displayQRCodeModal()}>
+              Get QR Code
+            </Button>
           </div>
         )}
       </Container>
     </>
   );
 };
-
 
 export default ArticleDetailPage;
